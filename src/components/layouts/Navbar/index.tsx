920c317c3de033a46220b5292a7eb217b9480364
adeganteng <wicaksonoade21@gmail.com>
2024-04-22 02:55:04 +0700

    Pages Router: 2. pages & Layout

---
+++ src/components/layouts/Navbar/index.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Navbar</h1>
+      <div>
+        <a href="#">Home</a>
+        <a href="/about">About</a>
+        <a href="/product">Product</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
